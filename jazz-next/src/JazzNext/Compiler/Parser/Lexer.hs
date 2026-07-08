12e0ae8daa4171e2aafbe9530140973bbe0df4b4
Ryan <Un3qual@users.noreply.github.com>
2026-07-08 10:37:13 -0700

    refactor(jazz-next): move lexer onto megaparsec

--- jazz-next/src/JazzNext/Compiler/Parser/Lexer.hs
+++ jazz-next/src/JazzNext/Compiler/Parser/Lexer.hs
@@ -8,7 +8,11 @@ module JazzNext.Compiler.Parser.Lexer
     tokenize
   ) where
 
+import Control.Applicative ((<|>))
+import Control.Monad (void)
 import Data.Char (isAlpha, isAlphaNum, isDigit, isSpace)
+import qualified Data.List.NonEmpty as NonEmpty
+import qualified Data.Set as Set
 import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.Read as TextRead
@@ -19,6 +23,22 @@ import JazzNext.Compiler.Diagnostics
   )
 import JazzNext.Compiler.Parser.Operator
   ( isStage2OperatorSymbolChar
+  )
+import Text.Megaparsec
+  ( Parsec
+  )
+import qualified Text.Megaparsec as MP
+import Text.Megaparsec.Char
+  ( char
+  )
+import Text.Megaparsec.Error
+  ( ErrorFancy (..),
+    ParseError (..),
+    ShowErrorComponent (..),
+    errorBundlePretty
+  )
+import Text.Megaparsec.Pos
+  ( unPos
   )
 
 -- | Token vocabulary understood by the current parser foundation.
@@ -58,202 +78,231 @@ data Token = Token
   }
   deriving (Eq, Show)
 
+data LexerError = LexerError Text
+  deriving (Eq, Ord, Show)
+
+instance ShowErrorComponent LexerError where
+  showErrorComponent (LexerError message) = Text.unpack message
+
+type LexerParser = Parsec LexerError Text
+
 -- Tokenizes the current parser foundation grammar while preserving 1-based
 -- line/column spans for diagnostics.
 tokenize :: Text -> Either Diagnostic [Token]
-tokenize = go 1 1
+tokenize source =
+  case MP.runParser (skipIgnored *> lexerTokens <* MP.eof) "jazz-next source" source of
+    Right tokens -> Right tokens
+    Left bundle -> Left (parseDiagnostic (lexerErrorMessage bundle))
+
+lexerTokens :: LexerParser [Token]
+lexerTokens =
+  MP.many (tokenParser <* skipIgnored)
+
+skipIgnored :: LexerParser ()
+skipIgnored =
+  MP.skipMany (void (MP.satisfy isSpace) <|> lineComment)
+
+lineComment :: LexerParser ()
+lineComment = do
+  void (char '#')
+  void (MP.takeWhileP (Just "comment") (/= '\n'))
+
+tokenParser :: LexerParser Token
+tokenParser = do
+  position <- MP.getSourcePos
+  let spanValue = sourcePosSpan position
+  intToken spanValue
+    <|> identifierToken spanValue
+    <|> symbolToken spanValue
+
+intToken :: SourceSpan -> LexerParser Token
+intToken spanValue = do
+  digits <- MP.takeWhile1P (Just "integer literal") isDigit
+  value <- parseIntegerLiteral spanValue digits
+  pure
+    Token
+      { tokenKind = TInt value,
+        tokenLexeme = digits,
+        tokenSpan = spanValue
+      }
+
+identifierToken :: SourceSpan -> LexerParser Token
+identifierToken spanValue = do
+  firstChar <- MP.satisfy isIdentifierStart
+  rest <- MP.takeWhileP (Just "identifier character") isIdentifierContinuation
+  let ident = Text.cons firstChar rest
+  pure
+    Token
+      { tokenKind = identifierKind ident,
+        tokenLexeme = ident,
+        tokenSpan = spanValue
+      }
+
+symbolToken :: SourceSpan -> LexerParser Token
+symbolToken spanValue = do
+  nextChar <- MP.lookAhead MP.anySingle
+  case nextChar of
+    ':' ->
+      fixedToken TColonColon "::" spanValue <|> fixedToken TColon ":" spanValue
+    '@' -> fixedToken TAt "@" spanValue
+    '=' ->
+      operatorToken "==" spanValue
+        <|> operatorToken "=>" spanValue
+        <|> fixedToken TEquals "=" spanValue
+    '!' ->
+      operatorToken "!=" spanValue <|> operatorRunToken spanValue
+    '<' ->
+      operatorToken "<=" spanValue <|> operatorRunToken spanValue
+    '>' ->
+      operatorToken ">=" spanValue <|> operatorRunToken spanValue
+    '+' -> operatorRunToken spanValue
+    '-' -> operatorOrArrowRunToken spanValue
+    '*' -> operatorRunToken spanValue
+    '/' -> operatorRunToken spanValue
+    '|' -> operatorRunToken spanValue
+    '%' -> operatorRunToken spanValue
+    '&' -> operatorRunToken spanValue
+    '?' -> operatorRunToken spanValue
+    '^' -> operatorRunToken spanValue
+    '~' -> operatorRunToken spanValue
+    '$' -> operatorToken "$" spanValue
+    '\\' -> fixedToken TLambda "\\" spanValue
+    '.' -> fixedToken TDot "." spanValue
+    '{' -> fixedToken TLBrace "{" spanValue
+    '}' -> fixedToken TRBrace "}" spanValue
+    '(' -> fixedToken TLParen "(" spanValue
+    ')' -> fixedToken TRParen ")" spanValue
+    '[' -> fixedToken TLBracket "[" spanValue
+    ']' -> fixedToken TRBracket "]" spanValue
+    ',' -> fixedToken TComma "," spanValue
+    _ -> unexpectedCharacter spanValue nextChar
+
+fixedToken :: TokenKind -> Text -> SourceSpan -> LexerParser Token
+fixedToken kind lexeme spanValue = do
+  void (MP.chunk lexeme)
+  pure
+    Token
+      { tokenKind = kind,
+        tokenLexeme = lexeme,
+        tokenSpan = spanValue
+      }
+
+operatorToken :: Text -> SourceSpan -> LexerParser Token
+operatorToken symbol spanValue = do
+  void (MP.chunk symbol)
+  pure
+    Token
+      { tokenKind = TOperator symbol,
+        tokenLexeme = symbol,
+        tokenSpan = spanValue
+      }
+
+operatorRunToken :: SourceSpan -> LexerParser Token
+operatorRunToken spanValue = do
+  symbol <- MP.takeWhile1P (Just "operator") isStage2OperatorSymbolChar
+  pure
+    Token
+      { tokenKind = TOperator symbol,
+        tokenLexeme = symbol,
+        tokenSpan = spanValue
+      }
+
+operatorOrArrowRunToken :: SourceSpan -> LexerParser Token
+operatorOrArrowRunToken spanValue = do
+  symbol <- MP.takeWhile1P (Just "operator") isStage2OperatorSymbolChar
+  pure
+    Token
+      { tokenKind =
+          case symbol of
+            "->" -> TArrow
+            _ -> TOperator symbol,
+        tokenLexeme = symbol,
+        tokenSpan = spanValue
+      }
+
+unexpectedCharacter :: SourceSpan -> Char -> LexerParser a
+unexpectedCharacter spanValue charValue =
+  MP.customFailure
+    ( LexerError
+        ( "unexpected character '"
+            <> Text.singleton charValue
+            <> "' at "
+            <> renderSpanValue spanValue
+        )
+    )
+
+identifierKind :: Text -> TokenKind
+identifierKind ident =
+  case ident of
+    "module" -> TModule
+    "import" -> TImport
+    "as" -> TAs
+    "data" -> TData
+    "if" -> TIf
+    "else" -> TElse
+    "case" -> TCase
+    _ -> TIdentifier ident
+
+isIdentifierStart :: Char -> Bool
+isIdentifierStart charValue = isAlpha charValue || charValue == '_'
+
+isIdentifierContinuation :: Char -> Bool
+isIdentifierContinuation charValue =
+  isAlphaNum charValue || charValue == '_' || charValue == '\'' || charValue == '!'
+
+sourcePosSpan :: MP.SourcePos -> SourceSpan
+sourcePosSpan sourcePosition =
+  SourceSpan
+    (unPos (MP.sourceLine sourcePosition))
+    (unPos (MP.sourceColumn sourcePosition))
+
+lexerErrorMessage :: MP.ParseErrorBundle Text LexerError -> Text
+lexerErrorMessage bundle =
+  case firstCustomLexerError bundle of
+    Just message -> message
+    Nothing -> Text.pack (errorBundlePretty bundle)
+
+firstCustomLexerError :: MP.ParseErrorBundle Text LexerError -> Maybe Text
+firstCustomLexerError bundle =
+  firstJust (map customErrorMessage (NonEmpty.toList (MP.bundleErrors bundle)))
   where
-    go :: Int -> Int -> Text -> Either Diagnostic [Token]
-    go line column source =
-      case Text.uncons source of
-        Nothing -> Right []
-        Just (char, rest)
-          | char == '\n' -> go (line + 1) 1 rest
-          | char == '\t' -> go line (column + tabStep column) rest
-          | isSpace char -> go line (column + 1) rest
-          -- `# ...` comments run to end-of-line and are ignored by the parser.
-          | char == '#' ->
-              let (commentTail, trailing) = Text.break (== '\n') rest
-               in go line (column + 1 + Text.length commentTail) trailing
-          | isDigit char ->
-              let (digits, trailing) = Text.span isDigit source
-                  width = Text.length digits
-               in do
-                    value <- parseIntLiteral line column digits
-                    let token =
-                          Token
-                            { tokenKind = TInt value,
-                              tokenLexeme = digits,
-                              tokenSpan = SourceSpan line column
-                            }
-                    (token :) <$> go line (column + width) trailing
-          | isIdentifierStart char ->
-              let (ident, trailing) = Text.span isIdentifierContinuation source
-                  width = Text.length ident
-                  kind =
-                    case ident of
-                      "module" -> TModule
-                      "import" -> TImport
-                      "as" -> TAs
-                      "data" -> TData
-                      "if" -> TIf
-                      "else" -> TElse
-                      "case" -> TCase
-                      _ -> TIdentifier ident
-                  token =
-                    Token
-                      { tokenKind = kind,
-                        tokenLexeme = ident,
-                        tokenSpan = SourceSpan line column
-                      }
-               in (token :) <$> go line (column + width) trailing
-          | otherwise ->
-              case char of
-                ':' ->
-                  case Text.uncons rest of
-                    Just (':', after) ->
-                      withSingleToken TColonColon "::" 2 line column after
-                    _ -> withSingleToken TColon ":" 1 line column rest
-                '@' -> withSingleToken TAt "@" 1 line column rest
-                '=' ->
-                  case Text.uncons rest of
-                    Just ('=', after) -> withOperatorToken "==" 2 line column after
-                    Just ('>', after) -> withOperatorToken "=>" 2 line column after
-                    _ -> withSingleToken TEquals "=" 1 line column rest
-                '!' ->
-                  case Text.uncons rest of
-                    Just ('=', after) -> withOperatorToken "!=" 2 line column after
-                    _ -> withOperatorRun line column source
-                '<' ->
-                  case Text.uncons rest of
-                    Just ('=', after) -> withOperatorToken "<=" 2 line column after
-                    _ -> withOperatorRun line column source
-                '>' ->
-                  case Text.uncons rest of
-                    Just ('=', after) -> withOperatorToken ">=" 2 line column after
-                    _ -> withOperatorRun line column source
-                '+' -> withOperatorRun line column source
-                '-' -> withOperatorOrArrowRun line column source
-                '*' -> withOperatorRun line column source
-                '/' -> withOperatorRun line column source
-                '|' -> withOperatorRun line column source
-                '%' -> withOperatorRun line column source
-                '&' -> withOperatorRun line column source
-                '?' -> withOperatorRun line column source
-                '^' -> withOperatorRun line column source
-                '~' -> withOperatorRun line column source
-                '$' -> withOperatorToken "$" 1 line column rest
-                '\\' -> withSingleToken TLambda "\\" 1 line column rest
-                '.' -> withSingleToken TDot "." 1 line column rest
-                '{' -> withSingleToken TLBrace "{" 1 line column rest
-                '}' -> withSingleToken TRBrace "}" 1 line column rest
-                '(' -> withSingleToken TLParen "(" 1 line column rest
-                ')' -> withSingleToken TRParen ")" 1 line column rest
-                '[' -> withSingleToken TLBracket "[" 1 line column rest
-                ']' -> withSingleToken TRBracket "]" 1 line column rest
-                ',' -> withSingleToken TComma "," 1 line column rest
-                _ ->
-                  Left
-                    ( parseDiagnostic
-                        ( "unexpected character '"
-                            <> Text.singleton char
-                            <> "' at "
-                            <> renderSpan line column
-                        )
-                    )
+    customErrorMessage parseError =
+      case parseError of
+        FancyError _ fancyErrors ->
+          firstJust
+            [ Just message
+              | ErrorCustom (LexerError message) <- Set.toList fancyErrors
+            ]
+        TrivialError {} -> Nothing
 
-    withSingleToken ::
-      TokenKind ->
-      Text ->
-      Int ->
-      Int ->
-      Int ->
-      Text ->
-      Either Diagnostic [Token]
-    withSingleToken kind lexeme width line column trailing =
-      let token =
-            Token
-              { tokenKind = kind,
-                tokenLexeme = lexeme,
-                tokenSpan = SourceSpan line column
-              }
-       in (token :) <$> go line (column + width) trailing
-
-    withOperatorToken ::
-      Text ->
-      Int ->
-      Int ->
-      Int ->
-      Text ->
-      Either Diagnostic [Token]
-    withOperatorToken symbol width line column trailing =
-      let token =
-            Token
-              { tokenKind = TOperator symbol,
-                tokenLexeme = symbol,
-                tokenSpan = SourceSpan line column
-              }
-       in (token :) <$> go line (column + width) trailing
-
-    withOperatorRun ::
-      Int ->
-      Int ->
-      Text ->
-      Either Diagnostic [Token]
-    withOperatorRun line column source =
-      let (symbol, trailing) = Text.span isStage2OperatorSymbolChar source
-          width = Text.length symbol
-       in withOperatorToken symbol width line column trailing
-
-    withOperatorOrArrowRun ::
-      Int ->
-      Int ->
-      Text ->
-      Either Diagnostic [Token]
-    withOperatorOrArrowRun line column source =
-      let (symbol, trailing) = Text.span isStage2OperatorSymbolChar source
-          width = Text.length symbol
-       in case symbol of
-            "->" -> withSingleToken TArrow symbol width line column trailing
-            _ -> withOperatorToken symbol width line column trailing
-
-    isIdentifierStart :: Char -> Bool
-    isIdentifierStart char = isAlpha char || char == '_'
-
-    isIdentifierContinuation :: Char -> Bool
-    isIdentifierContinuation char =
-      isAlphaNum char || char == '_' || char == '\'' || char == '!'
-
-    tabWidth :: Int
-    tabWidth = 8
-
-    -- Keep tab alignment deterministic across lexer and parser span tests.
-    tabStep :: Int -> Int
-    tabStep column = tabWidth - ((column - 1) `mod` tabWidth)
+    firstJust values =
+      case values of
+        [] -> Nothing
+        Just value : _ -> Just value
+        Nothing : rest -> firstJust rest
 
 -- | Render a compact source position for lexer diagnostics before full span
 -- rendering is available at this phase.
-renderSpan :: Int -> Int -> Text
-renderSpan line column = Text.pack (show line) <> ":" <> Text.pack (show column)
+renderSpanValue :: SourceSpan -> Text
+renderSpanValue (SourceSpan line column) =
+  Text.pack (show line) <> ":" <> Text.pack (show column)
 
--- | Parse source integer literals into unbounded `Integer` values so width-
--- specific signatures can validate the full declared numeric domain.
-parseIntLiteral :: Int -> Int -> Text -> Either Diagnostic Integer
-parseIntLiteral line column digits =
+parseIntegerLiteral :: SourceSpan -> Text -> LexerParser Integer
+parseIntegerLiteral spanValue digits =
   case TextRead.decimal digits :: Either String (Integer, Text) of
     Right (value, trailing)
-      | Text.null trailing -> Right value
-      | otherwise -> invalidIntegerDiagnostic digits line column
-    Left _ -> invalidIntegerDiagnostic digits line column
+      | Text.null trailing -> pure value
+      | otherwise -> invalidIntegerLiteral digits spanValue
+    Left _ -> invalidIntegerLiteral digits spanValue
 
-invalidIntegerDiagnostic :: Text -> Int -> Int -> Either Diagnostic a
-invalidIntegerDiagnostic digits line column =
-  Left
-    ( parseDiagnostic
+invalidIntegerLiteral :: Text -> SourceSpan -> LexerParser a
+invalidIntegerLiteral digits spanValue =
+  MP.customFailure
+    ( LexerError
         ( "invalid integer literal '"
             <> digits
             <> "' at "
-            <> renderSpan line column
+            <> renderSpanValue spanValue
         )
     )
 
