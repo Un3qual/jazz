7b87b295f3590bf7357bb04737034024ff25d339
Ryan <Un3qual@users.noreply.github.com>
2026-07-15 16:39:25 -0700

    refactor bundled prelude capability model

--- jazz-next/src/JazzNext/Compiler/BundledPrelude.hs
+++ jazz-next/src/JazzNext/Compiler/BundledPrelude.hs
@@ -4,17 +4,22 @@
 -- supply an explicit prelude file.
 module JazzNext.Compiler.BundledPrelude
   ( bundledPreludeSource,
-    loadBundledPreludeSource
-  ) where
+    loadBundledPreludeSource,
+  )
+where
 
 import Data.Text (Text)
 import qualified Data.Text as Text
+import JazzNext.Compiler.AST
+  ( NumericType (..),
+  )
 import JazzNext.Compiler.BuiltinCatalog
   ( BuiltinOwnership (PreludeTarget),
     allBuiltinSymbols,
-    builtinSymbolOwnership,
     builtinSymbolKernelName,
-    builtinSymbolName
+    builtinSymbolName,
+    builtinSymbolOwnership,
+    renderNumericTypeName,
   )
 
 -- | Pre-generated prelude text that exposes all builtin kernel bridges and
@@ -23,101 +28,16 @@ bundledPreludeSource :: Text
 bundledPreludeSource =
   Text.unlines $
     ["data Ordering = LT | EQ | GT.", ""]
-      <> map renderCapabilityClass canonicalCapabilityClassNames
+      <> map renderCapabilityClass canonicalCapabilityClasses
       <> ["", compareTextBinding]
       <> [""]
-      <> map renderDefaultCapabilityImpl defaultCapabilityImplFacts
+      <> map renderDefaultCapabilityImpl defaultCapabilityImpls
       <> [""]
       <> map renderKernelBridge allBuiltinSymbols
       <> [""]
       <> map renderPublicAlias preludeTargetSymbols
       <> map renderDefaultConversionAlias defaultConversionAliases
   where
-    renderCapabilityClass name =
-      case name of
-        "Eq" ->
-          Text.intercalate
-            "\n"
-            [ "class Eq(a) {",
-              "equals :: a -> a -> Bool.",
-              "}."
-            ]
-        "Ord" ->
-          Text.intercalate
-            "\n"
-            [ "class Ord(a) {",
-              "compare :: a -> a -> Ordering.",
-              "}."
-            ]
-        "Showable" ->
-          Text.intercalate
-            "\n"
-            [ "class Showable(a) {",
-              "show :: a -> Text.",
-              "}."
-            ]
-        "Default" ->
-          Text.intercalate
-            "\n"
-            [ "class Default(a) {",
-              "defaultValue :: a.",
-              "}."
-            ]
-        _ ->
-          "class " <> name <> "(a) { }."
-
-    renderDefaultCapabilityImpl (className, targetType) =
-      case (className, targetType) of
-        ("Eq", targetType') ->
-          renderEqImpl targetType'
-        ("Ord", "Char") ->
-          renderMethodImpl
-            "Ord"
-            "Char"
-            "compare"
-            "\\(left, right) -> if __kernel_charToUInt32 left < __kernel_charToUInt32 right then LT else if __kernel_charToUInt32 left > __kernel_charToUInt32 right then GT else EQ"
-        ("Ord", "Text") ->
-          renderMethodImpl "Ord" "Text" "compare" "__prelude_compareText"
-        ("Ord", targetType') ->
-          renderMethodImpl
-            "Ord"
-            targetType'
-            "compare"
-            "\\(left, right) -> if left < right then LT else if left > right then GT else EQ"
-        ("Showable", targetType') ->
-          renderMethodImpl "Showable" targetType' "show" "__kernel_renderValue"
-        ("Default", targetType') ->
-          renderMethodImpl "Default" targetType' "defaultValue" (defaultValueExpression targetType')
-        _ ->
-          "impl " <> className <> "(" <> targetType <> ") { }."
-
-    renderEqImpl targetType =
-      Text.intercalate
-        "\n"
-        [ "impl Eq(" <> targetType <> ") {",
-          "equals = \\(left, right) -> left == right.",
-          "}."
-        ]
-
-    renderMethodImpl className targetType methodName methodExpression =
-      Text.intercalate
-        "\n"
-        [ "impl " <> className <> "(" <> targetType <> ") {",
-          methodName <> " = " <> methodExpression <> ".",
-          "}."
-        ]
-
-    defaultValueExpression targetType =
-      case targetType of
-        "Float" -> "0.0"
-        "Float16" -> "__kernel_toFloat16 0"
-        "Float32" -> "__kernel_toFloat32 0"
-        "Float64" -> "__kernel_toFloat64 0"
-        "Bool" -> "False"
-        "Char" -> "'\\0'"
-        "Text" -> "\"\""
-        _ -> "0"
-
     -- Kernel bridge bindings must precede public aliases so alias definitions
     -- can reference already-declared names in the checked-in mirror.
     renderKernelBridge symbol =
@@ -132,6 +52,184 @@ bundledPreludeSource =
 
     preludeTargetSymbols =
       filter ((== PreludeTarget) . builtinSymbolOwnership) allBuiltinSymbols
+
+data CapabilityClass
+  = EqualityClass
+  | OrderingClass
+  | NumericClass
+  | IntegralClass
+  | FractionalClass
+  | ShowableClass
+  | DefaultClass
+
+data PreludeTargetType
+  = PreludeInt
+  | PreludeFloat
+  | PreludeBool
+  | PreludeChar
+  | PreludeText
+  | PreludeNumeric NumericType
+
+data OrderedPreludeTargetType
+  = OrderedPreludeInt
+  | OrderedPreludeFloat
+  | OrderedPreludeChar
+  | OrderedPreludeText
+  | OrderedPreludeNumeric NumericType
+
+data MarkerCapability
+  = NumericMarker
+  | IntegralMarker
+  | FractionalMarker
+
+data DefaultCapabilityImpl
+  = EqualityImpl PreludeTargetType
+  | OrderingImpl OrderedPreludeTargetType
+  | MarkerImpl MarkerCapability PreludeTargetType
+  | ShowableImpl PreludeTargetType
+  | DefaultImpl PreludeTargetType
+
+renderCapabilityClass :: CapabilityClass -> Text
+renderCapabilityClass capabilityClass =
+  case capabilityClass of
+    EqualityClass ->
+      renderClassWithMethod "Eq" "equals :: a -> a -> Bool."
+    OrderingClass ->
+      renderClassWithMethod "Ord" "compare :: a -> a -> Ordering."
+    NumericClass ->
+      renderMarkerClass "Num"
+    IntegralClass ->
+      renderMarkerClass "Integral"
+    FractionalClass ->
+      renderMarkerClass "Fractional"
+    ShowableClass ->
+      renderClassWithMethod "Showable" "show :: a -> Text."
+    DefaultClass ->
+      renderClassWithMethod "Default" "defaultValue :: a."
+
+renderClassWithMethod :: Text -> Text -> Text
+renderClassWithMethod className methodSignature =
+  Text.intercalate
+    "\n"
+    [ "class " <> className <> "(a) {",
+      methodSignature,
+      "}."
+    ]
+
+renderMarkerClass :: Text -> Text
+renderMarkerClass className =
+  "class " <> className <> "(a) { }."
+
+renderDefaultCapabilityImpl :: DefaultCapabilityImpl -> Text
+renderDefaultCapabilityImpl capabilityImpl =
+  case capabilityImpl of
+    EqualityImpl targetType ->
+      renderMethodImpl
+        "Eq"
+        (renderPreludeTargetType targetType)
+        "equals"
+        "\\(left, right) -> left == right"
+    OrderingImpl targetType ->
+      renderMethodImpl
+        "Ord"
+        (renderOrderedPreludeTargetType targetType)
+        "compare"
+        (orderingExpression targetType)
+    MarkerImpl marker targetType ->
+      renderEmptyImpl
+        (renderMarkerCapability marker)
+        (renderPreludeTargetType targetType)
+    ShowableImpl targetType ->
+      renderMethodImpl
+        "Showable"
+        (renderPreludeTargetType targetType)
+        "show"
+        "__kernel_renderValue"
+    DefaultImpl targetType ->
+      renderMethodImpl
+        "Default"
+        (renderPreludeTargetType targetType)
+        "defaultValue"
+        (defaultValueExpression targetType)
+
+renderMethodImpl :: Text -> Text -> Text -> Text -> Text
+renderMethodImpl className targetType methodName methodExpression =
+  Text.intercalate
+    "\n"
+    [ "impl " <> className <> "(" <> targetType <> ") {",
+      methodName <> " = " <> methodExpression <> ".",
+      "}."
+    ]
+
+renderEmptyImpl :: Text -> Text -> Text
+renderEmptyImpl className targetType =
+  "impl " <> className <> "(" <> targetType <> ") { }."
+
+renderPreludeTargetType :: PreludeTargetType -> Text
+renderPreludeTargetType targetType =
+  case targetType of
+    PreludeInt -> "Int"
+    PreludeFloat -> "Float"
+    PreludeBool -> "Bool"
+    PreludeChar -> "Char"
+    PreludeText -> "Text"
+    PreludeNumeric numericType -> renderNumericTypeName numericType
+
+renderOrderedPreludeTargetType :: OrderedPreludeTargetType -> Text
+renderOrderedPreludeTargetType targetType =
+  case targetType of
+    OrderedPreludeInt -> "Int"
+    OrderedPreludeFloat -> "Float"
+    OrderedPreludeChar -> "Char"
+    OrderedPreludeText -> "Text"
+    OrderedPreludeNumeric numericType -> renderNumericTypeName numericType
+
+renderMarkerCapability :: MarkerCapability -> Text
+renderMarkerCapability marker =
+  case marker of
+    NumericMarker -> "Num"
+    IntegralMarker -> "Integral"
+    FractionalMarker -> "Fractional"
+
+orderingExpression :: OrderedPreludeTargetType -> Text
+orderingExpression targetType =
+  case targetType of
+    OrderedPreludeChar ->
+      "\\(left, right) -> if __kernel_charToUInt32 left < __kernel_charToUInt32 right then LT else if __kernel_charToUInt32 left > __kernel_charToUInt32 right then GT else EQ"
+    OrderedPreludeText ->
+      "__prelude_compareText"
+    OrderedPreludeInt ->
+      numericOrderingExpression
+    OrderedPreludeFloat ->
+      numericOrderingExpression
+    OrderedPreludeNumeric _ ->
+      numericOrderingExpression
+
+numericOrderingExpression :: Text
+numericOrderingExpression =
+  "\\(left, right) -> if left < right then LT else if left > right then GT else EQ"
+
+defaultValueExpression :: PreludeTargetType -> Text
+defaultValueExpression targetType =
+  case targetType of
+    PreludeInt -> "0"
+    PreludeFloat -> "0.0"
+    PreludeBool -> "False"
+    PreludeChar -> "'\\0'"
+    PreludeText -> "\"\""
+    PreludeNumeric numericType ->
+      case numericType of
+        NumericInt8 -> "0"
+        NumericInt16 -> "0"
+        NumericInt32 -> "0"
+        NumericInt64 -> "0"
+        NumericUInt8 -> "0"
+        NumericUInt16 -> "0"
+        NumericUInt32 -> "0"
+        NumericUInt64 -> "0"
+        NumericFloat16 -> "__kernel_toFloat16 0"
+        NumericFloat32 -> "__kernel_toFloat32 0"
+        NumericFloat64 -> "__kernel_toFloat64 0"
 
 compareTextBinding :: Text
 compareTextBinding =
@@ -156,96 +254,96 @@ defaultConversionAliases =
     ("toFloat", "toFloat64")
   ]
 
-canonicalCapabilityClassNames :: [Text]
-canonicalCapabilityClassNames =
-  [ "Eq",
-    "Ord",
-    "Num",
-    "Integral",
-    "Fractional",
-    "Showable",
-    "Default"
+canonicalCapabilityClasses :: [CapabilityClass]
+canonicalCapabilityClasses =
+  [ EqualityClass,
+    OrderingClass,
+    NumericClass,
+    IntegralClass,
+    FractionalClass,
+    ShowableClass,
+    DefaultClass
   ]
 
-defaultCapabilityImplFacts :: [(Text, Text)]
-defaultCapabilityImplFacts =
-  defaultAliasCapabilityImplFacts
+defaultCapabilityImpls :: [DefaultCapabilityImpl]
+defaultCapabilityImpls =
+  defaultAliasCapabilityImpls
     <> concatMap integralNumericCapabilityImplFacts signedIntegerWidthTypes
     <> concatMap integralNumericCapabilityImplFacts unsignedIntegerWidthTypes
     <> concatMap floatingNumericCapabilityImplFacts floatingWidthTypes
 
-defaultAliasCapabilityImplFacts :: [(Text, Text)]
-defaultAliasCapabilityImplFacts =
-  [ ("Eq", "Int"),
-    ("Eq", "Float"),
-    ("Eq", "Bool"),
-    ("Eq", "Char"),
-    ("Eq", "Text"),
-    ("Ord", "Int"),
-    ("Ord", "Float"),
-    ("Ord", "Char"),
-    ("Ord", "Text"),
-    ("Num", "Int"),
-    ("Num", "Float"),
-    ("Integral", "Int"),
-    ("Fractional", "Float"),
-    ("Default", "Int"),
-    ("Default", "Float"),
-    ("Default", "Bool"),
-    ("Default", "Char"),
-    ("Default", "Text"),
-    ("Showable", "Int"),
-    ("Showable", "Float"),
-    ("Showable", "Bool"),
-    ("Showable", "Char"),
-    ("Showable", "Text")
+defaultAliasCapabilityImpls :: [DefaultCapabilityImpl]
+defaultAliasCapabilityImpls =
+  [ EqualityImpl PreludeInt,
+    EqualityImpl PreludeFloat,
+    EqualityImpl PreludeBool,
+    EqualityImpl PreludeChar,
+    EqualityImpl PreludeText,
+    OrderingImpl OrderedPreludeInt,
+    OrderingImpl OrderedPreludeFloat,
+    OrderingImpl OrderedPreludeChar,
+    OrderingImpl OrderedPreludeText,
+    MarkerImpl NumericMarker PreludeInt,
+    MarkerImpl NumericMarker PreludeFloat,
+    MarkerImpl IntegralMarker PreludeInt,
+    MarkerImpl FractionalMarker PreludeFloat,
+    DefaultImpl PreludeInt,
+    DefaultImpl PreludeFloat,
+    DefaultImpl PreludeBool,
+    DefaultImpl PreludeChar,
+    DefaultImpl PreludeText,
+    ShowableImpl PreludeInt,
+    ShowableImpl PreludeFloat,
+    ShowableImpl PreludeBool,
+    ShowableImpl PreludeChar,
+    ShowableImpl PreludeText
   ]
 
-integralNumericCapabilityImplFacts :: Text -> [(Text, Text)]
-integralNumericCapabilityImplFacts targetType =
-  map
-    (\className -> (className, targetType))
-    [ "Eq",
-      "Ord",
-      "Num",
-      "Integral",
-      "Default",
-      "Showable"
-    ]
+integralNumericCapabilityImplFacts :: NumericType -> [DefaultCapabilityImpl]
+integralNumericCapabilityImplFacts numericType =
+  [ EqualityImpl targetType,
+    OrderingImpl (OrderedPreludeNumeric numericType),
+    MarkerImpl NumericMarker targetType,
+    MarkerImpl IntegralMarker targetType,
+    DefaultImpl targetType,
+    ShowableImpl targetType
+  ]
+  where
+    targetType = PreludeNumeric numericType
 
-floatingNumericCapabilityImplFacts :: Text -> [(Text, Text)]
-floatingNumericCapabilityImplFacts targetType =
-  map
-    (\className -> (className, targetType))
-    [ "Eq",
-      "Ord",
-      "Num",
-      "Fractional",
-      "Default",
-      "Showable"
-    ]
+floatingNumericCapabilityImplFacts :: NumericType -> [DefaultCapabilityImpl]
+floatingNumericCapabilityImplFacts numericType =
+  [ EqualityImpl targetType,
+    OrderingImpl (OrderedPreludeNumeric numericType),
+    MarkerImpl NumericMarker targetType,
+    MarkerImpl FractionalMarker targetType,
+    DefaultImpl targetType,
+    ShowableImpl targetType
+  ]
+  where
+    targetType = PreludeNumeric numericType
 
-signedIntegerWidthTypes :: [Text]
+signedIntegerWidthTypes :: [NumericType]
 signedIntegerWidthTypes =
-  [ "Int8",
-    "Int16",
-    "Int32",
-    "Int64"
+  [ NumericInt8,
+    NumericInt16,
+    NumericInt32,
+    NumericInt64
   ]
 
-unsignedIntegerWidthTypes :: [Text]
+unsignedIntegerWidthTypes :: [NumericType]
 unsignedIntegerWidthTypes =
-  [ "UInt8",
-    "UInt16",
-    "UInt32",
-    "UInt64"
+  [ NumericUInt8,
+    NumericUInt16,
+    NumericUInt32,
+    NumericUInt64
   ]
 
-floatingWidthTypes :: [Text]
+floatingWidthTypes :: [NumericType]
 floatingWidthTypes =
-  [ "Float16",
-    "Float32",
-    "Float64"
+  [ NumericFloat16,
+    NumericFloat32,
+    NumericFloat64
   ]
 
 -- | IO wrapper kept for API symmetry with file-backed prelude loading paths.
