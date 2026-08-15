838462f8c6f7fa92e2878cd306e80374c93eb383
Ryan <Un3qual@users.noreply.github.com>
2026-08-15 14:36:45 -0700

    refactor: establish typed-core validator internals

--- src/Jazz/Compiler/TypedCore/Validate.hs
+++ src/Jazz/Compiler/TypedCore/Validate.hs
@@ -39,67 +39,7 @@ import Jazz.Compiler.CapabilityFacts (splitQualifiedMethodKey)
 import Jazz.Compiler.Name (operatorBindingIdentifierText)
 import Jazz.Compiler.Parser.Operator (isValidUserOperatorSymbol)
 import Jazz.Compiler.TypedCore
-
-data ModuleContext = ModuleContext
-  { moduleContextPath :: [Text],
-    moduleContextVisibleModules :: Set [Text],
-    moduleContextSchemes :: Map TypedBinderId TypedScheme,
-    moduleContextActiveSchemes :: Map ResolvedNameKey TypedScheme,
-    moduleContextVisibleNames :: Set ResolvedNameKey,
-    moduleContextSourceVisibleCapabilities :: Set ResolvedNameKey,
-    moduleContextVisibleImpls :: Set TypedImplId,
-    moduleContextImplMethods :: Map TypedImplId (Set Text),
-    moduleContextDataArities :: Map ResolvedNameKey Int,
-    moduleContextDataContracts :: Map ResolvedNameKey DataContract,
-    moduleContextConstructorContracts :: Map ResolvedNameKey ConstructorContract,
-    moduleContextCapabilityContracts :: Map ResolvedNameKey CapabilityContract,
-    moduleContextEvidenceCapabilities :: Map TypedEvidenceParameterRef ResolvedNameKey,
-    moduleContextLexicalContracts :: Map ResolvedNameKey BinderContract,
-    moduleContextTypeScope :: Set TypedTypeParameterId,
-    moduleContextPrimitiveConstraints :: [TypedPrimitiveConstraint]
-  }
-
-data ForwardSignedFunctionContext = ForwardSignedFunctionContext
-  { forwardFunctionSchemes :: Map TypedBinderId TypedScheme,
-    forwardFunctionActiveSchemes :: Map ResolvedNameKey TypedScheme,
-    forwardFunctionVisibleNames :: Set ResolvedNameKey
-  }
-
-data ResolvedNameKey
-  = ResolvedNameKey [Text] TypedNameNamespace Text
-  | GeneratedNameKey TypedGeneratedNameKind
-  deriving (Eq, Ord, Show)
-
-data BinderOccurrence = BinderOccurrence TypedCoreValidationPath TypedBinderId
-
-data BinderContract = BinderContract TypedBinderId TypedCoreName TypedType TypedRepresentationRecipe
-
-data PatternBinderContract = PatternBinderContract TypedBinderId TypedCoreName TypedType TypedRepresentationRecipe
-
-data ValueContract = ValueContract TypedType TypedRepresentationRecipe
-
-data ConstructorContract = ConstructorContract TypedBinderId ResolvedNameKey [TypedTypeParameterId] [TypedType]
-
-data DataContract = DataContract [TypedTypeParameterId] [[TypedType]]
-
-data CapabilityContract = CapabilityContract [TypedTypeParameterId] (Map Text TypedScheme)
-  deriving (Eq)
-
-data InstantiationContract
-  = InstantiationContract
-      TypedBinderId
-      [TypedTypeParameterId]
-      [TypedEvidenceParameter]
-      [TypedPrimitiveConstraint]
-
--- | Proof-carrying transport for internal producer-to-consumer paths. The
--- constructor stays private so externally built contract values must cross
--- 'validateTypedProgramOnce' before using a trusted consumer.
-newtype ValidatedTypedProgram = ValidatedTypedProgram TypedProgram
-  deriving (Eq, Show)
-
-validatedTypedProgram :: ValidatedTypedProgram -> TypedProgram
-validatedTypedProgram (ValidatedTypedProgram typedProgram) = typedProgram
+import Jazz.Compiler.TypedCore.Validate.Internal
 
 validateTypedProgramOnce :: TypedProgram -> Either [TypedCoreValidationFailure] ValidatedTypedProgram
 validateTypedProgramOnce typedProgram =
@@ -1257,51 +1197,6 @@ moduleExportsName namespace name exports =
   case coreNameIdentifier name of
     Nothing -> False
     Just identifier -> TypedModuleExport namespace identifier `elem` exports
-
-definitionNameKey :: [Text] -> TypedCoreName -> Maybe ResolvedNameKey
-definitionNameKey modulePath name =
-  case name of
-    TypedResolvedName _ namespace identifier -> Just (ResolvedNameKey modulePath namespace identifier)
-    TypedGeneratedName kind -> Just (GeneratedNameKey kind)
-    _ -> Nothing
-
-resolvedNameKey :: [Text] -> TypedCoreName -> Maybe ResolvedNameKey
-resolvedNameKey currentModulePath name =
-  case name of
-    TypedResolvedName origin namespace identifier ->
-      Just (ResolvedNameKey (originModulePath origin) namespace identifier)
-    TypedGeneratedName kind -> Just (GeneratedNameKey kind)
-    _ -> Nothing
-  where
-    originModulePath origin =
-      case origin of
-        TypedCurrentModule -> currentModulePath
-        TypedImportedModule modulePath -> modulePath
-        TypedAmbientPrelude -> ["Prelude"]
-
-withVisibleNames :: [TypedCoreName] -> ModuleContext -> ModuleContext
-withVisibleNames names context =
-  context
-    { moduleContextVisibleNames =
-        Set.union
-          (moduleContextVisibleNames context)
-          (Set.fromList [key | name <- names, key <- maybeToList (resolvedNameKey (moduleContextPath context) name)])
-    }
-
-withLexicalContracts :: [BinderContract] -> ModuleContext -> ModuleContext
-withLexicalContracts contracts context =
-  context
-    { moduleContextVisibleNames = Set.union localNames (moduleContextVisibleNames context),
-      moduleContextLexicalContracts = Map.union localContracts (moduleContextLexicalContracts context)
-    }
-  where
-    entries =
-      [ (key, contract)
-      | contract@(BinderContract _ name _ _) <- contracts,
-        key <- maybeToList (resolvedNameKey (moduleContextPath context) name)
-      ]
-    localNames = Set.fromList (map fst entries)
-    localContracts = Map.fromList entries
 
 withBlockDeclarations :: [TypedStatement] -> ModuleContext -> ModuleContext
 withBlockDeclarations statements context =
@@ -5624,13 +5519,6 @@ dataInterfaceConstructorMatches expected (TypedDataInterface (TypedDataDeclarati
   where
     constructorMatches (TypedConstructorDeclaration _ name _ _) = coreNameIdentifier name == Just expected
 
-coreNameIdentifier :: TypedCoreName -> Maybe Text
-coreNameIdentifier name =
-  case name of
-    TypedResolvedName _ _ identifier -> Just identifier
-    TypedBuiltinName identifier -> Just identifier
-    _ -> Nothing
-
 patternInfo :: TypedPattern -> TypedNodeInfo
 patternInfo patternValue =
   case patternValue of
@@ -5643,31 +5531,3 @@ patternInfo patternValue =
     TypedTuplePattern info _ -> info
     TypedAsPattern info _ _ _ -> info
     TypedOrPattern info _ -> info
-
-typedModulePath :: TypedModule -> [Text]
-typedModulePath (TypedModule modulePath _ _ _ _ _ _ _) = modulePath
-
-implModulePath :: TypedImplId -> [Text]
-implModulePath (TypedImplId modulePath _ _) = modulePath
-
-implTargetTypes :: TypedImplId -> [TypedType]
-implTargetTypes (TypedImplId _ _ targetTypes) = targetTypes
-
-renderModulePath :: [Text] -> Text
-renderModulePath = Text.intercalate "::"
-
-failure :: TypedCoreValidationPath -> TypedCoreValidationKind -> TypedCoreValidationDetail -> TypedCoreValidationFailure
-failure = TypedCoreValidationFailure
-
-maybeToList :: Maybe value -> [value]
-maybeToList maybeValue =
-  case maybeValue of
-    Nothing -> []
-    Just value -> [value]
-
-firstJust :: [Maybe value] -> Maybe value
-firstJust values =
-  case values of
-    [] -> Nothing
-    Nothing : rest -> firstJust rest
-    Just value : _ -> Just value
---
+++ src/Jazz/Compiler/TypedCore/Validate/Internal.hs
@@ -0,0 +1,182 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Shared contracts for the Typed Core validator implementation.
+--
+-- This module is private to the compiler library.  The public validator
+-- facade deliberately exports 'ValidatedTypedProgram' without its
+-- constructor, so construction of the proof remains tied to a successful
+-- validation pass.
+module Jazz.Compiler.TypedCore.Validate.Internal
+  ( ModuleContext (..),
+    ForwardSignedFunctionContext (..),
+    ResolvedNameKey (..),
+    BinderOccurrence (..),
+    BinderContract (..),
+    PatternBinderContract (..),
+    ValueContract (..),
+    ConstructorContract (..),
+    DataContract (..),
+    CapabilityContract (..),
+    InstantiationContract (..),
+    ValidatedTypedProgram (..),
+    validatedTypedProgram,
+    definitionNameKey,
+    resolvedNameKey,
+    withVisibleNames,
+    withLexicalContracts,
+    coreNameIdentifier,
+    typedModulePath,
+    implModulePath,
+    implTargetTypes,
+    renderModulePath,
+    failure,
+    maybeToList,
+    firstJust,
+  )
+where
+
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Jazz.Compiler.TypedCore
+
+data ModuleContext = ModuleContext
+  { moduleContextPath :: [Text],
+    moduleContextVisibleModules :: Set [Text],
+    moduleContextSchemes :: Map TypedBinderId TypedScheme,
+    moduleContextActiveSchemes :: Map ResolvedNameKey TypedScheme,
+    moduleContextVisibleNames :: Set ResolvedNameKey,
+    moduleContextSourceVisibleCapabilities :: Set ResolvedNameKey,
+    moduleContextVisibleImpls :: Set TypedImplId,
+    moduleContextImplMethods :: Map TypedImplId (Set Text),
+    moduleContextDataArities :: Map ResolvedNameKey Int,
+    moduleContextDataContracts :: Map ResolvedNameKey DataContract,
+    moduleContextConstructorContracts :: Map ResolvedNameKey ConstructorContract,
+    moduleContextCapabilityContracts :: Map ResolvedNameKey CapabilityContract,
+    moduleContextEvidenceCapabilities :: Map TypedEvidenceParameterRef ResolvedNameKey,
+    moduleContextLexicalContracts :: Map ResolvedNameKey BinderContract,
+    moduleContextTypeScope :: Set TypedTypeParameterId,
+    moduleContextPrimitiveConstraints :: [TypedPrimitiveConstraint]
+  }
+
+data ForwardSignedFunctionContext = ForwardSignedFunctionContext
+  { forwardFunctionSchemes :: Map TypedBinderId TypedScheme,
+    forwardFunctionActiveSchemes :: Map ResolvedNameKey TypedScheme,
+    forwardFunctionVisibleNames :: Set ResolvedNameKey
+  }
+
+data ResolvedNameKey
+  = ResolvedNameKey [Text] TypedNameNamespace Text
+  | GeneratedNameKey TypedGeneratedNameKind
+  deriving (Eq, Ord, Show)
+
+data BinderOccurrence = BinderOccurrence TypedCoreValidationPath TypedBinderId
+
+data BinderContract = BinderContract TypedBinderId TypedCoreName TypedType TypedRepresentationRecipe
+
+data PatternBinderContract = PatternBinderContract TypedBinderId TypedCoreName TypedType TypedRepresentationRecipe
+
+data ValueContract = ValueContract TypedType TypedRepresentationRecipe
+
+data ConstructorContract = ConstructorContract TypedBinderId ResolvedNameKey [TypedTypeParameterId] [TypedType]
+
+data DataContract = DataContract [TypedTypeParameterId] [[TypedType]]
+
+data CapabilityContract = CapabilityContract [TypedTypeParameterId] (Map Text TypedScheme)
+  deriving (Eq)
+
+data InstantiationContract
+  = InstantiationContract
+      TypedBinderId
+      [TypedTypeParameterId]
+      [TypedEvidenceParameter]
+      [TypedPrimitiveConstraint]
+
+newtype ValidatedTypedProgram = ValidatedTypedProgram TypedProgram
+  deriving (Eq, Show)
+
+validatedTypedProgram :: ValidatedTypedProgram -> TypedProgram
+validatedTypedProgram (ValidatedTypedProgram typedProgram) = typedProgram
+
+definitionNameKey :: [Text] -> TypedCoreName -> Maybe ResolvedNameKey
+definitionNameKey modulePath name =
+  case name of
+    TypedResolvedName _ namespace identifier -> Just (ResolvedNameKey modulePath namespace identifier)
+    TypedGeneratedName kind -> Just (GeneratedNameKey kind)
+    _ -> Nothing
+
+resolvedNameKey :: [Text] -> TypedCoreName -> Maybe ResolvedNameKey
+resolvedNameKey currentModulePath name =
+  case name of
+    TypedResolvedName origin namespace identifier ->
+      Just (ResolvedNameKey (originModulePath origin) namespace identifier)
+    TypedGeneratedName kind -> Just (GeneratedNameKey kind)
+    _ -> Nothing
+  where
+    originModulePath origin =
+      case origin of
+        TypedCurrentModule -> currentModulePath
+        TypedImportedModule modulePath -> modulePath
+        TypedAmbientPrelude -> ["Prelude"]
+
+withVisibleNames :: [TypedCoreName] -> ModuleContext -> ModuleContext
+withVisibleNames names context =
+  context
+    { moduleContextVisibleNames =
+        Set.union
+          (moduleContextVisibleNames context)
+          (Set.fromList [key | name <- names, key <- maybeToList (resolvedNameKey (moduleContextPath context) name)])
+    }
+
+withLexicalContracts :: [BinderContract] -> ModuleContext -> ModuleContext
+withLexicalContracts contracts context =
+  context
+    { moduleContextVisibleNames = Set.union localNames (moduleContextVisibleNames context),
+      moduleContextLexicalContracts = Map.union localContracts (moduleContextLexicalContracts context)
+    }
+  where
+    entries =
+      [ (key, contract)
+      | contract@(BinderContract _ name _ _) <- contracts,
+        key <- maybeToList (resolvedNameKey (moduleContextPath context) name)
+      ]
+    localNames = Set.fromList (map fst entries)
+    localContracts = Map.fromList entries
+
+coreNameIdentifier :: TypedCoreName -> Maybe Text
+coreNameIdentifier name =
+  case name of
+    TypedResolvedName _ _ identifier -> Just identifier
+    TypedBuiltinName identifier -> Just identifier
+    _ -> Nothing
+
+typedModulePath :: TypedModule -> [Text]
+typedModulePath (TypedModule modulePath _ _ _ _ _ _ _) = modulePath
+
+implModulePath :: TypedImplId -> [Text]
+implModulePath (TypedImplId modulePath _ _) = modulePath
+
+implTargetTypes :: TypedImplId -> [TypedType]
+implTargetTypes (TypedImplId _ _ targetTypes) = targetTypes
+
+renderModulePath :: [Text] -> Text
+renderModulePath = Text.intercalate "::"
+
+failure :: TypedCoreValidationPath -> TypedCoreValidationKind -> TypedCoreValidationDetail -> TypedCoreValidationFailure
+failure = TypedCoreValidationFailure
+
+maybeToList :: Maybe value -> [value]
+maybeToList maybeValue =
+  case maybeValue of
+    Nothing -> []
+    Just value -> [value]
+
+firstJust :: [Maybe value] -> Maybe value
+firstJust values =
+  case values of
+    [] -> Nothing
+    Nothing : rest -> firstJust rest
+    Just value : _ -> Just value
